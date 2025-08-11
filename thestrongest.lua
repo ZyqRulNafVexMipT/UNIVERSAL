0b68c3927f97ee3ac5cd41a2039bca8d7be81733
ZyqRulNafVexMipT <violettaaurora80@gmail.com>
2025-08-11 19:38:41 +0900

    Update thestrongest.lua

--- thestrongest.lua
+++ thestrongest.lua
@@ -5,7 +5,7 @@ local Window = Rayfield:CreateWindow({
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "VortX Hub | discord.gg/aVYJBJce",
    LoadingSubtitle = "By Owner Of VortX Hub",
-   ShowText = "gurt yo", -- for mobile users to unhide rayfield, change if you'd like
+   ShowText = "IDK", -- for mobile users to unhide rayfield, change if you'd like
    Theme = "Dark Blue", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
